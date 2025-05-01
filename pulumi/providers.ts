b92ad14c6f155927dd10717692a62951482957a0
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2025-05-01 11:30:01 -0600

    add port to teleport provider
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- pulumi/providers.ts
+++ pulumi/providers.ts
@@ -13,7 +13,7 @@ export const awsProvider = new aws.Provider("aws-default", {
 });
 
 export const teleportProvider = new teleport.Provider("mwi-demo", {
-  addr: "mwidemo.cloud.gravitational.io",
+  addr: "mwidemo.cloud.gravitational.io:443",
   joinMethod: "github",
   joinToken: "mwi-demo-aws-manager",
 })
